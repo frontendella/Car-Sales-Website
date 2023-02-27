2b8f64975712ed4a5d2629dd1d6d7ae7b5fe3852
frontendella <ryukhinaella@gmail.com>
2023-02-26 21:39:00 -0800

    Finished styling the webpage

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// javascript
+
 const menuBtn = document.querySelector('.menu')
       panel = document.querySelector('.filter')
-      panelStyles = window.getComputedStyle(panel);
+      panelStyles = window.getComputedStyle(panel)
+      hearts = document.querySelectorAll('.far');
       
 menuBtn.addEventListener('click', () => {
     
     if (panelStyles.getPropertyValue('clip-path') === 'circle(0% at 100% 0%)') {
         panel.style.clipPath = 'circle(100% at 50% 50%)'
     }
+    
     else {
         panel.style.clipPath = 'circle(0% at 100% 0%)'
     }
@@ -14,9 +18,12 @@ menuBtn.addEventListener('click', () => {
 
 document.addEventListener('mouseup', (event) => {
     if (panelStyles.getPropertyValue('clip-path') === 'circle(100% at 50% 50%)') {
-       if(!panel.contains(event.target)){
-            panel.style.clipPath = 'circle(0% at 100% 0%)'
-       }
+        if(!panel.contains(event.target))
+            panel.style.clipPath = 'circle(0% at 100% 0%'
     }
 })
 
+function changeHeart(x) {
+    x.classList.toggle("fas")
+    x.classList.toggle("far")
+}
